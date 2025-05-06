e8a56f99b446f4cb926d60f1f79bea7edd92d5b8
Mr-Fiddlez <126890289+Intermentality@users.noreply.github.com>
2025-05-06 13:50:51 -0400

    Deploy Website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
+  basePath: "/ela25_springwebsite",
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 
   //Compiler for quick loading. [Enable during production lol.]
