1fbebe4e866566b984ff58b079975e3bd1bb2816
Mr-Fiddlez <126890289+Intermentality@users.noreply.github.com>
2025-05-06 14:45:17 -0400

    NextJS 5 update

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -3,13 +3,11 @@ import { getArticles, getArticleBySlug } from "../fetchers";
 export async function generateStaticParams(){
     return getArticles()
 }
-export default async function ArticlePage({
-    params,
-}: {
-    params: { slug: string }
-}) {
-    const awaitParams = await params
-    const article = await getArticleBySlug(awaitParams.slug)
+
+type Params = Promise<{ slug: string }>
+export default async function ArticlePage({params}:{params:Params}){
+    const { slug } = await params
+    const article = await getArticleBySlug(slug)
     return <>
         <div className="relative grid place-items-center">
             <article className="my-4 p-2 w-full max-w-sm md:max-w-2xl">
